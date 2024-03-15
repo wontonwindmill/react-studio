53f094f5de31c916594c10a73cdf3943a8630fb7
wontonwindmill <winstonkhackett@gmail.com>
2024-03-14 23:06:22 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -8,23 +11,53 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [total, setTotal] = useState(0)
+  const [cartItems, setCartItems] = useState([])
+  const addCart = (name, price) => {
+    setTotal(total+price)
+    setCartItems(cartItems => [...cartItems, name])
+  }
+  const cartItemsDict = () => {
+    const dict = {}
+    cartItems.forEach(item => {
+      dict[item] = (dict[item] || 0) + 1
+    })
+    return dict
+  }
 
+  
   return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+    <ChakraProvider>
+      <div className="App">
+        {/* TODO: personalize your bakery (if you want) */}
+        {/*<p>{cartItems.length}</p>*/}
+        
+        <main className="Main">
+          <h1>My Bakery</h1>
+          <div className="ItemContainer">
+            {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+              //<p>{item.image}</p>
+              //<p>{item}</p>
+              <BakeryItem func={addCart} image={item.image} name={item.name} price={item.price} description={item.description}/>// replace with BakeryItem component
+            ))}
+          </div>
+        </main>
+      
+        <div>
+          <h2>Cart</h2>
+          {Object.keys(cartItemsDict()).map((item) => (
+            <p>{cartItemsDict()[item]}x {item}</p> 
+          ))}
+          {total == 0 ? <p>Nothing here just yet!</p> : <p>Total: {total}</p>}  
+        </div>
       </div>
-    </div>
+    </ChakraProvider>
+    
   );
 }
 
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,29 @@
 // TODO: create a component that displays a single bakery item
+import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Button, ButtonGroup} from '@chakra-ui/react'
+
+export default function BakeryItem(props){
+    return(
+        <Card maxW='sm'>
+            <CardBody>
+                <Image src={props.image} alt={props.name} borderRadius='lg'/>
+                <Stack mt='6' spacing='3'>
+                    <Heading size='md'>{props.name}</Heading>
+                    <Text>
+                       {props.description}
+                    </Text>
+                    
+                </Stack>
+            </CardBody>
+            <CardFooter>
+                <ButtonGroup spacing='175'>
+                <Text color='blue.600' fontSize='2xl'>
+                        {props.price}
+                    </Text>
+                <Button variant='ghost' colorScheme='blue' onClick={() => props.func(props.name, props.price)}>
+                    Add to cart
+                </Button>
+                </ButtonGroup>
+            </CardFooter>
+        </Card> 
+    )
+}
